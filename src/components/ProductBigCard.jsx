ffe3cc045777112c4fda59ab19ba261ebdfffe6d
ryukyunghye <ryukyung123@naver.com>
2023-01-13 21:43:03 +0900

    feat: 상품 카드, 상품 큰 카드 추가

--- src/components/ProductBigCard.jsx
+++ src/components/ProductBigCard.jsx
@@ -1,33 +1,34 @@
 import styled from "styled-components";
-const ProductCard = ({ thumbnail, title, desc, onClick }) => {
+const ProductBigCard = ({ thumbnail, title, desc, onClick }) => {
   return (
-    <ProductCardStyled onClick={onClick}>
+    <ProductBigCardStyled onClick={onClick}>
       <ProductThumbnail style={{ backgroundImage: `url(${thumbnail})` }} />
       <ProductTitle>{title}</ProductTitle>
       <ProductDesc>{desc}</ProductDesc>
-    </ProductCardStyled>
+    </ProductBigCardStyled>
   );
 };
 
-const ProductCardStyled = styled.div`
-  padding: 50px 24px;
+const ProductBigCardStyled = styled.div`
+  padding-bottom: 24px;
 `;
 const ProductThumbnail = styled.div`
-  width: 341px;
-  height: 240px;
+  width: 390px;
+  height: 420px;
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
 `;
 const ProductTitle = styled.div`
-  padding: 12px 0px;
+  padding: 12px 24px;
   font-weight: 700;
   font-size: 20px;
   line-height: 26px;
 `;
 const ProductDesc = styled.div`
+  padding-left: 24px;
   font-weight: 400;
   font-size: 16px;
   line-height: 21px;
 `;
-export default ProductCard;
+export default ProductBigCard;
