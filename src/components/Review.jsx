13909e5e8d5e6d6da01e02b8980b9f20f202ac03
ryukyunghye <ryukyung123@naver.com>
2023-01-13 21:47:20 +0900

    feat: 리뷰 추가

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -15,22 +15,30 @@ const Review = ({ profileImg, score, name, date, desc }) => {
 };
 
 const ReviewStyled = styled.div`
-  background-color: #eee;
+  background-color: #fafafa;
   width: 340px;
   height: 85px;
   margin-bottom: 15px;
+  margin-left: 30px;
   display: flex;
   align-items: center;
-  gap: 3px;
+  flex-direction: column;
 `;
 const ReviewCover = styled.div`
-  width: 100%;
-  height: 70%;
+  width: calc(100% - 60px);
+  height: 50%;
+  margin-top: 10px;
   display: flex;
 `;
 const ReviewImg = styled.div`
-  width: 30%;
-  border-radius: 10px;
+  width: 35px;
+  height: 35px;
+  border-radius: 12px;
+  background-image: url("https://file.namu.moe/file/8de19da261c4f463a5778501b02e33c33df5aaefb9f393c33caf1186dfddf5d1");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  margin-right: 10px;
 `;
 const ReviewScore = styled.div`
   color: yellow;
@@ -42,6 +50,10 @@ const ReviewInfo = styled.div`
 `;
 const ReviewDesc = styled.div`
   font-size: 13px;
+  width: calc(100% - 30px);
+  height: 50%;
+  text-align: left;
+  padding-left: 30px;
 `;
 
 export default Review;
